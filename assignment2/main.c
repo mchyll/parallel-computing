227a7f0e58f3c8515d1fa90cf69db704e2e70423
Magnus Conrad Hyll <magnus@hyll.no>
2019-09-07 00:02:06 +0200

    Removed unnecessary files + spaces over tabs

--- assignment2/main.c
+++ assignment2/main.c
@@ -12,68 +12,68 @@ int main() {
     // Set up MPI
     MPI_Init(NULL, NULL);
 
-	// Find the rank of this process and the number of processes
+    // Find the rank of this process and the number of processes
     int rank;
     int numProcesses;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-	printf("MPI is set up. My rank: %d, number of processes: %d\n", rank, numProcesses);
+    printf("MPI is set up. My rank: %d, number of processes: %d\n", rank, numProcesses);
 
-	// Make sure we can divide the image evenly
+    // Make sure we can divide the image evenly
     if (YSIZE % numProcesses != 0) {
         printf("Can't divide image in equal parts for each process. Try a number of processes which divides the number of rows in the image.\n");
         MPI_Finalize();
         return 1;
     }
 
-	// Find number of rows and bytes each worker should process
+    // Find number of rows and bytes each worker should process
     int rowsPerProcess = YSIZE / numProcesses;
     int bytesPerProcess = XSIZE * YSIZE * 3 / numProcesses;
 
     uchar* fullOriginalImage;
     uchar* localOriginalImage = calloc(XSIZE * rowsPerProcess * 3, 1);
 
-	// Main process reads the original image
+    // Main process reads the original image
     if (rank == 0) {
-		printf("Rank %d reading image\n", rank);
+        printf("Rank %d reading image\n", rank);
         fullOriginalImage = calloc(XSIZE * YSIZE * 3, 1); // Three uchars per pixel (RGB)
         readbmp("before.bmp", fullOriginalImage);
     }
 
-	// Main process scatters image data across processes,
-	// processes receives their part of the image into the localOriginalImage buffer
-	printf("Scattering in rank %d\n", rank);
+    // Main process scatters image data across processes,
+    // processes receives their part of the image into the localOriginalImage buffer
+    printf("Scattering in rank %d\n", rank);
     MPI_Scatter(fullOriginalImage, bytesPerProcess, MPI_UNSIGNED_CHAR, localOriginalImage, bytesPerProcess, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
-	// Each process modifies its part of the image
-	printf("Modifying local image in rank %d\n", rank);
+    // Each process modifies its part of the image
+    printf("Modifying local image in rank %d\n", rank);
     uchar* localModifiedImage = modifyImage(localOriginalImage, XSIZE, rowsPerProcess);
     free(localOriginalImage);
 
-	uchar* fullModifiedImage;
+    uchar* fullModifiedImage;
     if (rank == 0) {
-		// Only main process should allocate memory for the full modified image
+        // Only main process should allocate memory for the full modified image
         fullModifiedImage = calloc(4 * XSIZE * YSIZE * 3, 1);
     }
 
-	// Processes sends their part of the image from the localModifiedImage buffer,
-	// main process gathers all image data from the processes
-	printf("Gathering full image in rank %d\n", rank);
-	MPI_Gather(localModifiedImage, bytesPerProcess * 4, MPI_UNSIGNED_CHAR, fullModifiedImage, bytesPerProcess * 4, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    // Processes sends their part of the image from the localModifiedImage buffer,
+    // main process gathers all image data from the processes
+    printf("Gathering full image in rank %d\n", rank);
+    MPI_Gather(localModifiedImage, bytesPerProcess * 4, MPI_UNSIGNED_CHAR, fullModifiedImage, bytesPerProcess * 4, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
-	// Main process saves the full modified image
+    // Main process saves the full modified image
     if (rank == 0) {
-		printf("Saving full image in rank %d\n", rank);
+        printf("Saving full image in rank %d\n", rank);
         savebmp("after.bmp", fullModifiedImage, XSIZE * 2, YSIZE * 2);
 
-		// Free the memory allocated for the full original and modified images
-		free(fullOriginalImage);
-		free(fullModifiedImage);
+        // Free the memory allocated for the full original and modified images
+        free(fullOriginalImage);
+        free(fullModifiedImage);
     }
 
     free(localModifiedImage);
-	MPI_Finalize();
+    MPI_Finalize();
 
     return 0;
 }
